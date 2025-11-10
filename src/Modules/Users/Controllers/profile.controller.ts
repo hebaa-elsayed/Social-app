84ab0018a9f970b89d6991346ebbad0ddada4375
hebaa-elsayed <hebaa.elsayeddd@gmail.com>
2025-11-10 17:04:51 +0200

    adding friendship requests

--- src/Modules/Users/Controllers/profile.controller.ts
+++ src/Modules/Users/Controllers/profile.controller.ts
@@ -5,21 +5,36 @@ const profileController = Router();
 
 //  Get user profile
 profileController.get('/get-profile', authentication, profileService.getProfile)
+
 //  Update user profile
 profileController.put('/update-profile', authentication, profileService.updateProfile)
+
 //  Delete user profile
 profileController.delete('/delete-account', authentication, profileService.deleteAccount)
+
 //  Upload user profile picture
 profileController.post('/profile-picture', authentication, Multer().single('profilePicture'),profileService.uploadProfilePicture)
 
 //  Upload user cover picture
+profileController.post('/cover-picture', authentication, Multer().single('coverPicture'),profileService.uploadCoverPicture)
 
 //  List all users
 profileController.get('/list-users', authentication, profileService.listUsers)
+
 //  Renew signed url
 profileController.post('/renew-signed-url', authentication, profileService.renewSignedUrl)
 
 //  Upload large user profile picture
 // profileController.post('/large-profile-picture', authentication, Multer().single('profilePicture'),profileService.uploadLargeProfilePicture)
+
+//  Send friendship request
+profileController.post('/send-friendship-request', authentication, profileService.sendFriendshipRequest)
+
+// List friendship requests
+profileController.get('/list-requests', authentication, profileService.listRequests)
+
+// Respond to friendship request
+profileController.patch('/respond-to-friendship-request', authentication, profileService.respondToFriendshipRequest)
+
 
 export { profileController };
